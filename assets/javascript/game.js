64831c2e3ea62e1027aedeeb92a50576f5db5367
Abigail Nielson <aa.nielson18@gmail.com>
2020-03-14 21:11:31 -0600

    moved js link to the bottom of index and created variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,3 +7,13 @@ var bookList = [
     "Persuasion",
     "Lady Susan"
 ]
+//create array for possible words
+var wordBank = [
+    "pride", "prejudice", "persuasion", "emma", "sense", "sensibility"
+];
+//create variables for wins, losses, guesses made, guesses left, and letters guessed
+var wins = 0;
+var losses = 0;
+var guessed = 0;
+var guessesLeft = 15;
+var lettersGuessed = [];
