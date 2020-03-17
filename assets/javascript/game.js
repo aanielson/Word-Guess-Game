56869c4cabe662a910f1bf1d2f6c5eeccae72151
Abigail Nielson <aa.nielson18@gmail.com>
2020-03-16 18:36:04 -0600

    pushed userInput to lettersGuessed array and displayed it

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,43 @@ var wordBank = [
     "pride", "prejudice", "persuasion", "emma", "sense", "sensibility"
 ];
 //create variables for wins, losses, guesses made, guesses left, and letters guessed
-var word = "";
-var n;
-var blanks
+//choose a random word from the word bank
+var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+
 var wins = 0;
 var losses = 0;
+
 var guessed = 0;
 var guessesLeft = 15;
 var lettersGuessed = [];
 
-//choose a random word from the word bank
-function randomWord() {
-    word = wordBank[Math.floor(Math.random() * wordBank.length)];
-    console.log(word);
-    n = word.length;
-    blanks = document.getElementById("blanks");
-    
-}
-randomWord();
-console.log(n)
 //display the number of underscores for the number of letters in the selected word
+
+function displayWord () {
+    
+    blanks = document.getElementById("blanks");
+    blanks.innerHTML = word;
+};
+displayWord();
 
 //check to see what key has been pressed
 var userInput = document.getElementById("guessed-letters");
     document.onkeyup = function(event) {
-    userInput.textContent = event.key;
+    lettersGuessed.push(" " + event.key);
+    userInput.innerHTML = lettersGuessed;
     };
+
+
+//update guessed and guessesLeft
+function updateGuesses () {
+    for (var i = 0; i < lettersGuessed.length; i++) {
+       guessed += 1;
+       guessesLeft -= 1; 
+    };
+};
+updateGuesses();
 console.log(guessed);
 console.log(guessesLeft);
-//update guessed and guessesLeft
-
 //compare that letter with the letters of the word
 //if there is a match, fill in the underscore with that letter
 //if no match, display the letter in the lettersGuessed
