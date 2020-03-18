49efe47d9799c40a7c90314bdfdeb07894e95fff
Abigail Nielson <aa.nielson18@gmail.com>
2020-03-17 22:10:58 -0600

    made it so that you can only guess letters once

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,80 +4,96 @@ var wordBank = [
 ];
 //create variables for wins, losses, guesses made, guesses left, and letters guessed
 //choose a random word from the word bank
-var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+
 var wins = 0;
 var losses = 0;
 
 var guessed = 0;
 var guessesLeft = 15;
 var lettersGuessed = [];
+var doubleGuess = true;
 var blanks = document.getElementById("blanks");
 var guessesLeftDiv = document.getElementById("guesses-left");
-
+var wordArr;
 guessesLeftDiv.innerHTML = guessesLeft;
 
-var wordArr = displayWord();
+
 
 function displayWord () {
+    var word = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log(word);
     var wordArray = word.split(" ");
     for(var x = 0; x < wordArray.length; x++){
         blanks.innerHTML += '<span id="letter-'+x+'"> _ </span>';
     }
-    return wordArray;
+    wordArr = wordArray;
 };
-console.log(word);
+displayWord();
 
 //check to see what key has been pressed
 var userInput = document.getElementById("guessed-letters");
 document.onkeyup = function(event) {
-    lettersGuessed.push(" " + event.key);
+    
     userInput.innerHTML = lettersGuessed;
-        
+    var checkLetter = " " + event.key;   
+    console.log("the letter pressed is " + checkLetter);
+    for (var i = 0; i < lettersGuessed.length; i++) {
+       console.log(lettersGuessed[i]);
+        if (checkLetter == lettersGuessed[i]) {
+            alert("You've already guessed that key!");
+            doubleGuess = false;
+        };
+    }
+
     // The letter could exist
         // letter gets replaced
         // we win
+    lettersGuessed.push(" " + event.key);
     var doesExist = wordArr.indexOf(event.key);
     console.log("break 1");
     // if letter hasn't already been pressed
     // if we win, we need to reset the game
     // if we lose, we need to reset the game
-    if(doesExist > -1){
-        var pos = 0;
-        var counter = -1;
-        
-        while (pos != -1){
-            pos = wordArr.indexOf(event.key, counter+1);
-            counter = pos;
-            if (pos != -1) {
-            var replaceText = document.getElementById("letter-" + pos);
-            replaceText.innerHTML = event.key;
-            guessed++;
+    if (doubleGuess === true) {
+        if(doesExist > -1){
+            var pos = 0;
+            var counter = -1;
+            
+            while (pos != -1){
+                pos = wordArr.indexOf(event.key, counter+1);
+                counter = pos;
+                if (pos != -1) {
+                var replaceText = document.getElementById("letter-" + pos);
+                replaceText.innerHTML = event.key;
+                guessed++;
+                }
             }
+            document.getElementById("guesses").innerHTML = guessed;
+            //if guessed === number of items in the array
+                //wins++ and reset game
+            
+        }  else {
+            // The letter doesn't exist
+            // no guessed left - we lose
+            // guesses left - we lose a guess
+            if (guessesLeft < 1) {
+                losses++;
+                document.getElementById("losses").innerHTML = losses;
+                reset();
+            }
+            // else there are guesses left, just remove a guess
+            else { 
+                guessesLeft--; 
+                guessesLeftDiv.innerHTML = guessesLeft;
+            }  
         }
-        document.getElementById("guesses").innerHTML = guessed;
-        //if guessed === number of items in the array
-            //wins++ and reset game
-        
-    }  else {
-        // The letter doesn't exist
-        // no guessed left - we lose
-        // guesses left - we lose a guess
-        if (guessesLeft < 1) {
-            losses++;
-            document.getElementById("losses").innerHTML = losses;
-            reset();
-        }
-        // else there are guesses left, just remove a guess
-         else { 
-            guessesLeft--; 
-            guessesLeftDiv.innerHTML = guessesLeft;
-        }  
     }
     if (guessed >= wordArr.length) {
         wins++;
         document.getElementById("wins").innerHTML = wins;
         reset();
     }
+    doubleGuess = true;
 };
 //function for reseting the whole page
 function reset() {
@@ -87,7 +103,6 @@ function reset() {
     document.getElementById("guesses-left").innerHTML = guessesLeft;
     lettersGuessed = [];
     document.getElementById("guessed-letters").innerHTML = lettersGuessed;
-    word = wordBank[Math.floor(Math.random() * wordBank.length)];
     blanks.innerHTML = "";
     displayWord();
 }
